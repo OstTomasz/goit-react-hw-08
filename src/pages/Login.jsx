4275c0de529644b8f71e0b3909f136ffb255bc78
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2025-01-05 16:01:45 +0100

    added wrong login handling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,16 @@
+import { useSelector } from "react-redux";
 import { LoginForm } from "../components/LoginForm/LoginForm";
+import { selectAuthentication } from "../redux/selectors";
+import { Loader } from "../components/Loader";
 
 export const LoginPage = () => {
+  const { isLoading, error } = useSelector(selectAuthentication);
   return (
     <div>
       <h1>Login Page</h1>
       <LoginForm />
+      {isLoading && <Loader />}
+      {error && <p>There is no user with this credentials!</p>}
     </div>
   );
 };
